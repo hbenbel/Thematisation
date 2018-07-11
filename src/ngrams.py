7eb636b235af78a3590c9d3f4b0ce5bf58e0b09a
Thibaud Chominot <thibaud.chominot@epita.fr>
2018-07-11 12:23:12 +0200

    Add frequency of ngrams counter

---
+++ src/ngrams.py
@@ -0,0 +1,24 @@
+from collections import Counter
+from functools import reduce
+
+def shingles(words, k=3):
+  for i in range(len(words) - k + 1):
+    yield ' '.join(words[i:i+k])
+
+def words(doc_str):
+  return doc_str.split(" ") # maybe can be improved ?
+
+def count_ngrams(docs, k_grams=3):
+  if isinstance(docs, list):
+    counters = map(lambda doc: count_ngrams(doc, k_grams), docs)
+    return reduce(lambda acc, new: acc + new, counters, Counter())
+  else:
+    return Counter(shingles(words(docs), k_grams))
+
+def freqs(docs, k_grams=3):
+  counter = count_ngrams(docs, k_grams)
+  counts = counter.values()
+  ngrams = counter.keys()
+  total = sum(counts)
+  freqs = map(lambda c: c / total, counts)
+  return list(zip(ngrams, freqs));
