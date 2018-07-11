02afd3a6e5da62e4f223d55a650448f978e5a5a0
sirink <sirine.kefi@epita.fr>
2018-07-11 12:54:25 +0200

    [MAIN] Correction of small errors

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import classUtils
+import os.path
 
 def loadFile(path):
     with open(path, encoding='utf-8', errors='ignore') as f:
@@ -8,21 +9,25 @@ def loadFile(path):
 
 def getThematics():
     thematicsList = []
-    nbThematics = input("How many thematics do yout want to enter ? ")
+    nbThematics = raw_input("How many thematics do yout want to enter ? ")
     for i in range(int(nbThematics)):
-        name = input("Enter the name of the thematics " + str(i) + ": ")
-        thematic = classUtils.Thematic(name, "")
-        thematicsList.append(thematic)
+      name = raw_input("Enter the name of the thematics " + str(i) + ": ")
+      thematic = classUtils.Thematic(name, "")
+      thematicsList.append(thematic)
 
-    for thematic in thematicsList:
-        option = input("Enter a text or load an existing text ? (1 = Manually; 2 = Load File)")
-        if (option == "1"):
-            text = input("Enter a text for the thematic " + thematic.name + ": ")
-            thematic.text = text
-        elif (option == "2"):
-            filepath = input("Enter the path of the file to load: ")
-            thematic.text = loadFile(filepath)
-        else:
-            print("Wrong option :(")
-
+      for thematic in thematicsList:
+	option = raw_input("Enter a text or load an existing text ? (1 = Manually; 2 = Load File) ")
+	if (option == "1"):
+          text = raw_input("Enter a text for the thematic " + thematic.name + ": ")
+	  thematic.text = text
+	elif (option == "2"):
+	  filepath = raw_input("Enter the path of the file to load: ")
+	  if (os.path.exists(filepath)):
+	    thematic.text = loadFile(filepath)
+	  else:
+	    print("Wrong path :/")
+	else:
+	  print("Wrong option :(")
     return thematicsList
+
+getThematics()
