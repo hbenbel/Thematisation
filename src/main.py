a97419a9b4b870399ba232be85faa4becf106908
Hussem Ben Belgacem <benbel_h@epita.fr>
2018-07-12 17:04:07 +0200

    [THAMTIZATION] Adding a non optimized thematization with dicts

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import classUtils
 import ngrams
 import os.path
+import operator
 
 def loadFile(path):
     with open(path, encoding='utf-8', errors='ignore') as f:
@@ -35,7 +36,7 @@ def validationNgrams(theme):
     newNgram = []
     for f in fr:
       print(f)
-      validation = input("Do you want to validate this n-gram? (yes/no)")
+      validation = input("Do you want to validate this n-gram? (yes/no): ")
       if (validation.strip() == "yes" or validation.strip() == "y"):
         newNgram.append(f)
         print("This n-gram has been validated")
@@ -47,6 +48,30 @@ def validationNgrams(theme):
 
 
 thematics = getThematics()
+listofngram = []
+listofthematics = []
 for theme in thematics:
+  print("\n\n")
+  print("Validation for the theme", theme.name)
   newNgram = validationNgrams(theme)
-  print("\n",newNgram, "\n")
+  for ngram in newNgram:
+      listofngram.append((ngram[0], theme.name))
+      listofthematics.append((theme.name, 0))
+
+dictngram = dict(listofngram)
+dictofthematics = dict(listofthematics)
+print(listofngram)
+
+print("\n\n")
+path = input("Enter the path of the text to thematize: ")
+if (os.path.exists(path)):
+    text = loadFile(path)
+else:
+    print("Wront path :/")
+
+fr = ngrams.freqs(text)
+for f in fr:
+    if f[0] in dictngram:
+        dictofthematics[f[1]] += 1
+
+print("The closest category for this text is: ", max(dictofthematics.items(), key=operator.itemgetter(1))[0])
